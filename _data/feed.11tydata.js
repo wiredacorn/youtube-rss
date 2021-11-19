bfb08c9153011c276fbee962b2895580028d0090
wiredacorn <dev@wiredacorn.com>
2021-11-19 02:29:52 -0700

    aspect ratio

--- _data/feed.11tydata.js
+++ _data/feed.11tydata.js
@@ -6,8 +6,8 @@ let parser = new Parser({
 });
 
 module.exports = (async () => {
-
-  let feed = await parser.parseURL('https://www.youtube.com/feeds/videos.xml?channel_id=UCU0jDHRcI7NcOO3rJd9NCJg');
+  xmlSource = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCU0jDHRcI7NcOO3rJd9NCJg'
+  let feed = await parser.parseURL('https://www.scriptbarrel.com/xml.cgi?channel_id=UCU0jDHRcI7NcOO3rJd9NCJg&name=Vintage%20City%20Church');
   
   console.log(feed.items[0]['media:group']['media:thumbnail'][0]['$'].url);
 
